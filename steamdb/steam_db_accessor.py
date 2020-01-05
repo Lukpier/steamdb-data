34b140084fc0287f9b21cc8d06863034671cbe28
Luca Pierri <lukepier95@gmail.com>
2020-01-05 11:26:20 +0100

    wip crawlying bypass

--- steamdb/steam_db_accessor.py
+++ steamdb/steam_db_accessor.py
@@ -2,6 +2,21 @@ from common.requests_api import RequestsApi
 from steamdb.query_mode import QueryMode
 app_id = "292030" # the witcher 3 app id on steam
 import json
+import random
+
+user_agent_list = [
+   #Chrome
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
+  ]
 
 class SteamDBAccessor:
     def __init__(self):
@@ -9,7 +24,7 @@ class SteamDBAccessor:
         self.dataAccessor = RequestsApi('steamdb.info')
         self.dataAccessorHeaders = {
             'Accept': 'application/json',
-            'Referer': 'https://steamdb.info/',
+            'Referer': 'https://steamdb.info/'
         }
 
     def getAppIds(self):
@@ -19,11 +34,18 @@ class SteamDBAccessor:
     def getPlayersHistory(self, app_id: str, mode: QueryMode) -> dict:
         _type = "concurrent_week" if (mode == QueryMode.WEEKLY) else "concurrent_max"
         service_url = '/api/GetGraph/?type={}&appid={}'.format(_type, app_id)
-        r = self.dataAccessor.get(service_url, headers = self.dataAccessorHeaders)
+        r = self.dataAccessor.get(service_url, headers = self.randomUserAgentToHeaders(user_agent_list))
+        print(r)
         return json.loads(r)
 
     def getPricesHistory(self, app_id: str, currency="eu") -> dict:
         service_url = '/api/GetPriceHistory/?appid={}&cc={}'.format(app_id, currency)
         r = self.dataAccessor.get(service_url, headers = self.dataAccessorHeaders)
         return json.loads(r)
+    
+    def randomUserAgentToHeaders(self, user_agent_list: list) -> dict:
+        user_agent = random.choice(user_agent_list)
+        headers = self.dataAccessorHeaders
+        headers['User-Agent'] = user_agent
+        return headers
 
--- steamdb/steam_db_service.py
+++ steamdb/steam_db_service.py
@@ -1,10 +1,14 @@
 from steamdb.steam_db_accessor import SteamDBAccessor
 from steamdb.query_mode import QueryMode
 import logging
+import time
 
 logger = logging.getLogger(__name__)
 logging.basicConfig()
 logger.setLevel(logging.DEBUG)
+
+concurrent = 200
+batch = 900
 
 class SteamDBService:
 
@@ -15,18 +19,34 @@ class SteamDBService:
         logger.info('Retrieving all app ids from steam api...')
         apps = self.accessor.getAppIds()['applist']['apps']
         logger.info('Done. Start collecting players history for %s apps', len(apps))
+        return self.getPlayersHistories(apps)
+
+    def getPlayersHistories(self, apps: list) -> list:
         res = []
-        for app in apps:
-            r = {}
+        for i, app in enumerate(apps):
             name = app['name']
             _id = app['appid']
             logger.info('Querying steam db api for %s', name)
-            history = self.accessor.getPlayersHistory(_id, QueryMode.WEEKLY)
-            if (history['success']):
-                r['app_name'] = name
-                r['app_id'] = _id
-                r['players_history'] = history['data']
-                res.append(r)
-            else: 
-                logger.info('No players history data for %s', name)
+            self.getPlayersHistory(_id, name)
+            print(i)
+            if (i != 0) and (i % 900) == 0:
+                logger.info('Waiting 10 seconds to prevent crawl..')
+                time.sleep(30)
+            else:
+                time.sleep(0.05)
         return res
+
+    def getPlayersHistory(self, app_id: str, app_name: str) -> dict:
+        history = self.accessor.getPlayersHistory(app_id, QueryMode.FULL)
+        if (history['success']):
+            return self.mkRes(app_id, app_name, history)
+        else: 
+            logger.info('No players history data for %s', app_name)
+            return {}
+
+    def mkRes(self, app_id: str, app_name: str, history: dict):
+        r = {}
+        r['app_name'] = app_name
+        r['app_id'] = app_id
+        r['players_history'] = history['data']
+        return r
